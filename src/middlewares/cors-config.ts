0d4537c6f27b22551ce7fb33e3c4c5c0883a6c35
mohamed-nawas <mgmnawas@gmail.com>
2024-11-21 13:54:56 +0530

    temp

--- src/middlewares/cors-config.ts
+++ src/middlewares/cors-config.ts
@@ -8,7 +8,7 @@ const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || "http://localhost:5173";
 module.exports = async (app: Application) => {
 
     app.use((req: Request, res: Response, next: NextFunction): void => {
-        res.header("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+        res.header("Access-Control-Allow-Origin", "https://pft-client.vercel.app");
         res.header("Access-Control-Allow-Credentials", "true");
         if (req.method === "OPTIONS") {
             res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
