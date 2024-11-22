d31a6cb84310278d477296c398e172ef52d77bad
mohamed-nawas <mgmnawas@gmail.com>
2024-11-22 21:12:28 +0530

    change

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+require('dotenv').config();
+import express, { Application } from "express";
+
+const expressApp = require('./express-app');
+
+const StartServer = async() => {
+
+    // initialize the application server
+    const app: Application = express();
+    const port = process.env.PORT || 3000;
+    
+    await expressApp(app);
+
+    // listen to the application server on PORT
+    app.listen(port, (): void => {
+        console.log(`Express application server running on address => http://localhost:${port}`);
+    });
+}
+
+StartServer();
