282aea50f141006d125b0a2777283a1bc4886a70
mohamed-nawas <mgmnawas@gmail.com>
2024-11-30 17:41:15 +0530

    added keys to env as base64 and read as utf-8 for prod

--- api/services/JwtService.ts
+++ api/services/JwtService.ts
@@ -6,8 +6,25 @@ const jwt = require('jsonwebtoken');
 /**
  * Jwt service
  */
-var PRIVATE_KEY = process.env.PRIVATE_KEY || fs.readFileSync('api/keys/private.key', 'utf8');
-var PUBLIC_KEY = process.env.PUBLIC_KEY || fs.readFileSync('api/keys/public.key', 'utf8');
+const getPrivateKey = (): string => {
+    if (process.env.ENVIRONMENT === 'DEVELOPMENT') return fs.readFileSync('api/keys/private.key', 'utf-8');
+    if (process.env.ENVIRONMENT === 'PRODUCTION') {
+        const base64key = process.env.PRIVATE_KEY_B64;
+        if (!base64key) throw new PFTException('PRIVATE_KEY_B64 environment variable is not set');
+        return Buffer.from(base64key, 'base64').toString('utf-8');
+    };
+    throw new PFTException('The enviroment is not valid to generate RSA keys');
+}
+
+const getPublicKey = (): string => {
+    if (process.env.ENVIRONMENT === 'DEVELOPMENT') return fs.readFileSync('api/keys/public.key', 'utf-8');
+    if (process.env.ENVIRONMENT === 'PRODUCTION') {
+        const base64key = process.env.PUBLIC_KEY_B64;
+        if (!base64key) throw new PFTException('PUBLIC_KEY_B64 environment variable is not set');
+        return Buffer.from(base64key, 'base64').toString('utf-8');
+    };
+    throw new PFTException('The enviroment is not valid to generate RSA keys');
+}
 
 export const TOKEN_ISSUER = 'Computic Solutions';
 export const TOKEN_SUBJECT = 'Auth token';
@@ -25,12 +42,11 @@ export function sign(payload: any, $Option: JwtSignVerifyOptions): boolean {
         subject: $Option.subject,
         audience: $Option.audience,
         expiresIn: '1h',
-        // algorithm: 'RS256'      // use for dev
-        algorithm: 'HS256'      // use for prod with vercel, since there is an issue with asymettric key passing as env in vercel
+        algorithm: 'RS256'
     };
 
     try {
-        return jwt.sign(payload, PRIVATE_KEY, signOptions);
+        return jwt.sign(payload, getPrivateKey(), signOptions);
     } catch (e) {
         console.error(e);
         throw new PFTException("Jwt sign in failed", e as Error);
@@ -43,12 +59,11 @@ export function verify(token: string, $Option: JwtSignVerifyOptions): boolean {
         subject: $Option.subject,
         audience: $Option.audience,
         expiresIn: '1h',
-        // algorithm: ['RS256']        // use for dev
-        algorithm: ['HS256']        // use for prod with vercel, since there is an issue with asymettric key passing as env in vercel
+        algorithm: ['RS256']
     };
 
     try {
-        return jwt.verify(token, PUBLIC_KEY, verifyOptions);
+        return jwt.verify(token, getPublicKey(), verifyOptions);
     } catch (e) {
         console.error(e);
         throw new PFTException("Jwt verification failed", e as Error);
